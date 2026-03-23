a127487491c48cba53f870e0d9fc9eb6b37f01ce
ssantossiemens <sara.santos.ext@siemens.com>
2026-03-23 15:51:24 +0000

    Update streamlit_app.py
    Let's Put the JSON into a Dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 # Import python packages.
 import streamlit as st
 from snowflake.snowpark.functions import col
-import requests
-
 
 # Write directly to the app.
 st.title(f" :cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -44,5 +42,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
 
+import requests  
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
